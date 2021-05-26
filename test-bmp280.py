ae9ac1ee8592fb81b7246b372567ddee1c821e5d
Jacob Walser <jwalser90@gmail.com>
2021-05-26 16:36:22 -0400

    test-bmp280: add proper signal handling

--- test-bmp280.py
+++ test-bmp280.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
-from bmp280 import BMP280
 import argparse
+from bmp280 import BMP280
+import signal
 import time
 
 parser = argparse.ArgumentParser(description='bmp280 test')
@@ -11,20 +12,26 @@ args = parser.parse_args()
 
 bmp = BMP280()
 
+outfile = None
+
 if args.output:
     outfile = open(args.output, "w")
+
+def cleanup(_signo, _stack):
+    if outfile:
+        outfile.close()
+    exit(0)
+
+
+signal.signal(signal.SIGTERM, cleanup)
+signal.signal(signal.SIGINT, cleanup)
 
 while True:
     data = bmp.get_data()
     output = f"{time.time()} 1 {data.pressure} {data.temperature} {data.pressure_raw} {data.temperature_raw}"
     print(output)
-    if args.output:
+    if outfile:
         outfile.write(output)
         outfile.write('\n')
     if args.frequency:
         time.sleep(1.0/args.frequency)
-
-# this is never reached, but works anyway in practice
-# todo handle KeyboardInterrupt for ctrl+c
-if args.output:
-    outfile.close()
