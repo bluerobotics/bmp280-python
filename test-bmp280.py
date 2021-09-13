5b6cbe5b20710d1d62ecb5d7d2671c4f63a0d342
ES-Alexander <sandman.esalexander@gmail.com>
2021-09-13 15:37:19 +1000

    test-bmp280: use context manager, pathlib

--- test-bmp280.py
+++ test-bmp280.py
@@ -3,34 +3,24 @@
 import argparse
 from bmp280 import BMP280
 from llog import LLogWriter, LLOG_DATA
-import signal
 import time
-import os
+from pathlib import Path
 
-dir_path = os.path.dirname(os.path.realpath(__file__))
+device = "bmp280"
+defaultMeta = Path(__file__).resolve().parent / f"{device}.meta"
 
-defaultMeta = dir_path + '/bmp280.meta'
-
-parser = argparse.ArgumentParser(description='bmp280 test')
+parser = argparse.ArgumentParser(description=f'{device} test')
 parser.add_argument('--output', action='store', type=str, default=None)
 parser.add_argument('--meta', action='store', type=str, default=defaultMeta)
 parser.add_argument('--frequency', action='store', type=int, default=None)
 args = parser.parse_args()
 
-log = LLogWriter(args.meta, args.output)
+with LLogWriter(args.meta, args.output) as log:
+    bmp = BMP280()
 
-def cleanup(_signo, _stack):
-    log.close()
-    exit(0)
-
-signal.signal(signal.SIGTERM, cleanup)
-signal.signal(signal.SIGINT, cleanup)
-
-bmp = BMP280()
-
-while True:
-    data = bmp.get_data()
-    output = f"{data.pressure} {data.temperature} {data.pressure_raw} {data.temperature_raw}"
-    log.log(LLOG_DATA, output)
-    if args.frequency:
-        time.sleep(1.0/args.frequency)
+    while True:
+        data = bmp.get_data()
+        output = f"{data.pressure} {data.temperature} {data.pressure_raw} {data.temperature_raw}"
+        log.log(LLOG_DATA, output)
+        if args.frequency:
+            time.sleep(1.0/args.frequency)
