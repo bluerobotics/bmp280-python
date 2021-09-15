c468c6a7b1b0ec291258588ca8f317399072d064
Jacob Walser <jwalser90@gmail.com>
2021-09-15 00:21:49 -0400

    test-bmp280: log rom and compensation

--- bmp280/test-bmp280.py
+++ bmp280/test-bmp280.py
@@ -2,7 +2,7 @@
 
 import argparse
 from bmp280 import BMP280
-from llog import LLogWriter, LLOG_DATA
+from llog import LLogWriter, LLOG_DATA, LLOG_CONFIG, LLOG_ROM
 import time
 from pathlib import Path
 
@@ -17,10 +17,12 @@ args = parser.parse_args()
 
 with LLogWriter(args.meta, args.output) as log:
     bmp = BMP280()
-
+    compensation = bmp.get_compensation()
+    log.log(LLOG_ROM, ' '.join(str(d) for d in compensation.data))
+    log.log(LLOG_CONFIG, f'{bmp.osrs_t} {bmp.osrs_p} {bmp.mode} {bmp.t_sb} {bmp.filter}')
     while True:
         data = bmp.get_data()
-        output = f"{data.pressure} {data.temperature} {data.pressure_raw} {data.temperature_raw}"
+        output = f'{data.pressure} {data.temperature} {data.pressure_raw} {data.temperature_raw}'
         log.log(LLOG_DATA, output)
         if args.frequency:
             time.sleep(1.0/args.frequency)
