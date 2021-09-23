58a7e2b8a3ff07b6eb1c22e2d24b65df32d75dce
ES-Alexander <sandman.esalexander@gmail.com>
2021-09-23 12:29:47 +1000

    report: use default parser

--- bmp280/report.py
+++ bmp280/report.py
@@ -2,10 +2,12 @@
 
 import matplotlib.pyplot as plt
 
-def generate_figures(log):
-    footer = 'bmp280 test report'
+DEVICE = 'bmp280'
 
-    f, spec = log.figure(height_ratios=[1,1], suptitle='bmp280 data', footer=footer)
+def generate_figures(log):
+    footer = f'{DEVICE} test report'
+
+    f, spec = log.figure(height_ratios=[1,1], suptitle=f'{DEVICE} data', footer=footer)
     plt.subplot(spec[0,0])
     log.rom.T.ttable(rl=True)
     plt.subplot(spec[0,1])
@@ -15,19 +17,10 @@ def generate_figures(log):
     log.data.pressure.pplot(log.data.temperature)
 
 if __name__ == '__main__':
-    import argparse
     from llog import LLogReader
     from matplotlib.backends.backend_pdf import PdfPages
-    import os
-
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-
-    defaultMeta = dir_path + '/bmp280.meta'
-    parser = argparse.ArgumentParser(description='bmp280 test report')
-    parser.add_argument('--input', action='store', type=str, required=True)
-    parser.add_argument('--meta', action='store', type=str, default=defaultMeta)
-    parser.add_argument('--output', action='store', type=str)
-    parser.add_argument('--show', action='store_true')
+    
+    parser = LLogReader.create_default_parser(__file__, DEVICE)
     args = parser.parse_args()
 
     log = LLogReader(args.input, args.meta)
