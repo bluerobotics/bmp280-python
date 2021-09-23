5263b2356b8ab62767b8d7b658b741bbc3f18bb6
ES-Alexander <sandman.esalexander@gmail.com>
2021-09-23 12:26:21 +1000

    test-bmp280: simplify with abstracted logging

--- bmp280/test.py
+++ bmp280/test.py
@@ -1,28 +1,19 @@
 #!/usr/bin/python3
 
-import argparse
 from bmp280 import BMP280
-from llog import LLogWriter, LLOG_DATA, LLOG_CONFIG, LLOG_ROM
-import time
-from pathlib import Path
+from llog import LLogWriter
 
 device = "bmp280"
-defaultMeta = Path(__file__).resolve().parent / f"{device}.meta"
-
-parser = argparse.ArgumentParser(description=f'{device} test')
-parser.add_argument('--output', action='store', type=str, default=None)
-parser.add_argument('--meta', action='store', type=str, default=defaultMeta)
-parser.add_argument('--frequency', action='store', type=int, default=None)
+parser = LLogWriter.create_default_parser(__file__, device)
 args = parser.parse_args()
 
 with LLogWriter(args.meta, args.output) as log:
     bmp = BMP280()
     compensation = bmp.get_compensation()
-    log.log(LLOG_ROM, ' '.join(str(d) for d in compensation.data))
-    log.log(LLOG_CONFIG, f'{bmp.osrs_t} {bmp.osrs_p} {bmp.mode} {bmp.t_sb} {bmp.filter}')
-    while True:
+    log.log_rom(' '.join(str(d) for d in compensation.data))
+    log.log_config(f'{bmp.osrs_t} {bmp.osrs_p} {bmp.mode} {bmp.t_sb} {bmp.filter}')
+    
+    def data_getter():
         data = bmp.get_data()
-        output = f'{data.pressure} {data.temperature} {data.pressure_raw} {data.temperature_raw}'
-        log.log(LLOG_DATA, output)
-        if args.frequency:
-            time.sleep(1.0/args.frequency)
+        return f'{data.pressure} {data.temperature} {data.pressure_raw} {data.temperature_raw}'
+    log.log_data_loop(data_getter, parser_args=args)
