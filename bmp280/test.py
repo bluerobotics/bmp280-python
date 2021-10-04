9b08e9adfa019fdb647e28704b3dd9dc5f87c301
Jacob Walser <jwalser90@gmail.com>
2021-10-04 15:47:09 -0400

    test: add i2c bus argument

--- bmp280/test.py
+++ bmp280/test.py
@@ -5,10 +5,11 @@ from llog import LLogWriter
 
 device = "bmp280"
 parser = LLogWriter.create_default_parser(__file__, device)
+parser.add_argument("--bus", default=1, help="i2c bus")
 args = parser.parse_args()
 
 with LLogWriter(args.meta, args.output, console=args.console) as log:
-    bmp = BMP280()
+    bmp = BMP280(args.bus)
     compensation = bmp.get_compensation()
     log.log_rom(' '.join(str(d) for d in compensation.data))
     log.log_config(f'{bmp.osrs_t} {bmp.osrs_p} {bmp.mode} {bmp.t_sb} {bmp.filter}')
